e15f25b3411135fec40b5a44e31185c49c9df6f4
Muhammad Imran <imran5244513@gmail.com>
2025-05-18 22:20:07 +0500

    update

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
-import Alert from './alert';
+
  
 export default function Home() {
   return (
