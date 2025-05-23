3d0c85ce702a0f32615f0d27b1dbb0e1282c0993
Muhammad Imran <imran5244513@gmail.com>
2025-05-23 16:14:10 +0500

    final code

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -10,18 +12,25 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          My name is Muhammad Imran. I have completed my BSCS from Virtual
+          University of Pakistan. I'm a passionate Frontend Developer with
+          experience in React, JavaScript, and Tailwind CSS. I have worked on
+          various responsive web projects, including e-commerce and restaurant
+          websites. I'm currently learning backend development to become a Full
+          Stack Developer.
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
